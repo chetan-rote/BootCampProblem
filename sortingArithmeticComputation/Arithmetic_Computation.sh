6d8a43b4672edb631e012782d7973eb7ccc74601
chetan-rote <rote.chetan@gmail.com>
2020-08-29 02:06:00 +0530

    Computation 2

--- sortingArithmeticComputation/Arithmetic_Computation.sh
+++ sortingArithmeticComputation/Arithmetic_Computation.sh
@@ -5,10 +5,15 @@
 #UC1
 
 # taking input from user
-read -p "enter first number 1:" num1
-read -p "enter second number 2:" num2
-read -p "enter third number 3:" num3
+read -p "enter first number 1:" number1
+read -p "enter second number 2:" number2
+read -p "enter third number 3:" number3
 
 #UC2Compute1
 Result1=$(($number1+$number2*$number3))
 echo "Result of operation1 is:"$Result1
+
+#UC3Compute2
+Result2=$(($number1*$number2+$number3))
+echo "Result of operation2 is:"$Result2
+
