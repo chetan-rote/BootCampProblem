d18f84b01910bd67e5a8c91abcbbea41db092f41
chetan-rote <rote.chetan@gmail.com>
2020-08-29 02:14:35 +0530

    Computation 3

--- sortingArithmeticComputation/Arithmetic_Computation.sh
+++ sortingArithmeticComputation/Arithmetic_Computation.sh
@@ -17,3 +17,6 @@ echo "Result of operation1 is:"$Result1
 Result2=$(($number1*$number2+$number3))
 echo "Result of operation2 is:"$Result2
 
+#UC4Compute3
+Result3=$(($number3+$number1/$number2))
+echo "Result of operation2 is:"$Result3
