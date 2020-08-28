8e1f42c9c81582199d4349cd8fbb301c4e22a3c3
chetan-rote <rote.chetan@gmail.com>
2020-08-29 00:22:16 +0530

    ADDED:Sorting Arithmatic Computations

--- Programming_Constructs/SortingArithmaticBash.sh
+++ Programming_Constructs/SortingArithmaticBash.sh
@@ -3,9 +3,9 @@
 echo "welcome to Artihmetic computation problem"
 
 # taking input from user
-read -p "enter first number:" num1
-read -p "enter second number:" num2
-read -p "enter third number:" num3
+read -p "enter first number 1:" num1
+read -p "enter second number 2:" num2
+read -p "enter third number 3:" num3
 
 #printing numbers entered by user
 echo "first number:"$num1
@@ -19,53 +19,53 @@ Result3=$(($num3+$num1/$num2))
 Result4=$(($num1%$num2+$num3))
 
 #delcaring a dictionary
-declare -A myDict
+declare -A Dictionary
 
 #storing computation data in dictionary
-myDict[cnt1]=$Result1
-myDict[cnt2]=$Result2
-myDict[cnt3]=$Result3
-myDict[cnt4]=$Result4
+Dictionary[cnt1]=$Result1
+Dictionary[cnt2]=$Result2
+Dictionary[cnt3]=$Result3
+Dictionary[cnt4]=$Result4
 
 #printing dictionary
-echo "Dictionary elements are:" ${myDict[@]}
+echo "Dictionary elements are:" ${Dictionary[@]}
 
 #storing dictionary in array
 count=0
-for i in ${myDict[@]}
+for i in ${Dictionary[@]}
 do
-	myArray[((count++))]=$i
+	array[((count++))]=$i
 done
 
 #printing array elements
-echo "Array elements are:" ${myArray[@]}
+echo "Array elements are:" ${array[@]}
 
 #sorting in descending order
 for((i=0;i<4;i++))
 do
 	for((j = 0; j<4-i-1; j++))
 	do
-		if [ ${myArray[j]} -lt ${myArray[$((j+1))]} ]
+		if [ ${array[j]} -lt ${array[$((j+1))]} ]
 		then
-			temp=${myArray[j]}
-			myArray[$j]=${myArray[$((j+1))]}
-			myArray[$((j+1))]=$temp
+			temp=${array[j]}
+			array[$j]=${array[$((j+1))]}
+			array[$((j+1))]=$temp
 		fi
 	done
 done
-echo "Sorted Array in descending order:" ${myArray[@]}
+echo "Sorted Array in descending order:" ${array[@]}
 
 #sorting in ascending order
 for((i=0;i<4;i++))
 do
 	for((j = 0; j<4-i-1; j++))
 	do
-		if [ ${myArray[j]} -gt ${myArray[$((j+1))]} ]
+		if [ ${array[j]} -gt ${array[$((j+1))]} ]
 		then
-			temp=${myArray[j]}
-			myArray[$j]=${myArray[$((j+1))]}
-			myArray[$((j+1))]=$temp
+			temp=${array[j]}
+			array[$j]=${array[$((j+1))]}
+			array[$((j+1))]=$temp
 		fi
 	done
 done
-echo "Sorted Array in ascending order:" ${myArray[@]}
+echo "Sorted Array in ascending order:" ${array[@]}
