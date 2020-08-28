ef06ed64eddae1054d6f96170613fa64b7852e62
chetan-rote <rote.chetan@gmail.com>
2020-08-29 02:54:22 +0530

    Sort values in descending order

--- sortingArithmeticComputation/Arithmetic_Computation.sh
+++ sortingArithmeticComputation/Arithmetic_Computation.sh
@@ -25,19 +25,20 @@ echo "Result of operation2 is:"$Result3
 Result4=$(($number1%$number2+$number3))
 echo "Result of operation4 is:"$Result4
 
-#UCStores computation in Dictionary
+
 #delcaring a dictionary
 declare -A Dictionary
 
+#UC6Stores computation in Dictionary
 #storing computation data in dictionary
 Dictionary[res1]=$Result1
 Dictionary[res2]=$Result2
 Dictionary[res3]=$Result3
 Dictionary[res4]=$Result4
 
-#UC7 Stores value in array
 echo "Dictionary elements are:" ${Dictionary[@]}
 
+#UC7 Stores value in array
 #storing dictionary in array
 count=0
 for i in ${Dictionary[@]}
@@ -45,4 +46,21 @@ do
 	array[((count++))]=$i
 done
 
+echo "Array elements are:" ${array[@]}
 
+
+#UC8Sort result in descending array
+
+for((i=0;i<4;i++))
+do
+	for((j = 0; j<4-i-1; j++))
+	do
+		if [ ${array[j]} -lt ${array[$((j+1))]} ]
+		then
+			temp=${array[j]}
+			array[$j]=${array[$((j+1))]}
+			array[$((j+1))]=$temp
+		fi
+	done
+done
+echo "Sorted Array in descending order:" ${array[@]}
